121d014904877912fa02eb58e28bc72c6504f1ae
Bitna Kim <tallshe108@hanmail.net>
2022-02-01 15:25:54 +0900

    firstcommit

---
+++ src/page/ProductAll.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProductCard from "../component/ProductCard";
+import { useSearchParams } from "react-router-dom";
+
+const ProductAll = () => {
+  let [productList, setProductList] = useState([]);
+  let [query, setQuery] = useSearchParams();
+
+  const getProducts = async () => {
+    let searchQuery = query.get("q") || "";
+    console.log("쿼리값은?", searchQuery);
+    let url = `https://my-json-server.typicode.com/legobitna/noona-hnm/products?q=${searchQuery}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    setProductList(data);
+  };
+  useEffect(() => {
+    getProducts();
+  }, [query]);
+  return (
+    <div>
+      <Container>
+        <Row>
+          {productList.map((menu) => (
+            <Col lg={3}>
+              <ProductCard item={menu} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default ProductAll;
---
+++ src/page/ProductDetail.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+
+const ProductDetail = () => {
+  let { id } = useParams();
+  let [product, setProduct] = useState(null);
+  const getProductDetail = async () => {
+    let url = `https://my-json-server.typicode.com/legobitna/noona-hnm/products/${id}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+    setProduct(data);
+  };
+  useEffect(() => {
+    getProductDetail();
+  }, []);
+
+  return (
+    <Container>
+      <Row>
+        <Col className="product-img">
+          <img src={product?.img} />
+        </Col>
+        <Col>
+          <div>{product?.title}</div>
+          <div>{product?.price}</div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default ProductDetail;
